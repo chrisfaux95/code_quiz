6bf54519e1a1ad9631a93c3f3e3eaf860389e336
Chris Faux <chrisfaux95@gmail.com>
2020-07-24 12:48:26 -0400

    added shuffle algorithm for scrambling questions

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+//function to shuffle the questions:
+//from: https://stackoverflow.com/a/12646864/13871979
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
