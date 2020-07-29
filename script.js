116b8517b48a9b050db7d6b4212fb2d05820408e
Chris Faux <chrisfaux95@gmail.com>
2020-07-29 14:25:22 -0400

    removed redundant code and added comments

--- script.js
+++ script.js
@@ -72,9 +72,9 @@ const questions = [
     }
 
 ]
-//setup variables:
+// Setup variables:
 var score = 0;
-//time in seconds
+// Time in seconds
 var maxTime = 100;
 
 
@@ -84,16 +84,16 @@ Prints the given question to the question box,
 and creates the buttons for the possible answers.
 */
 function printQuestion(q, qList) {
-    //set the current question to the header
+    // Set the current question to the header
     let currentQ = $("#question")
     currentQ.text(q.question);
-    //grab the list destination and empty it
+    // Grab the list destination and empty it
     var qListDiv = $("#answers-list");
     qListDiv.empty();
-    //create a copy of list of possible answers and randomize the order
+    // Create a copy of list of possible answers and randomize the order
     let answersList = [...q.answers];
     shuffleArray(answersList);
-    /*creates a button for each answer with
+    /* Creates a button for each answer with
     with bootstrap classes and appends it to the answers list*/
     answersList.forEach(ans => {
         let currentAns = $("<button>");
@@ -101,7 +101,7 @@ function printQuestion(q, qList) {
         currentAns.addClass("btn btn-primary btn-block answer-button");
         currentAns.appendTo(qListDiv);
     });
-    //creates event listeners for each button with the ability to check their answer.
+    // Creates event listeners for each button with the ability to check their answer.
     $(".answer-button").on("click", function () {
         var isCorrect = checkAnswer(q, $(this).text());
         console.log(isCorrect);
@@ -114,17 +114,15 @@ function printQuestion(q, qList) {
         nextQuestion(q, qList);
     });
 }
-//setting up initial question
-// var currentQuestion = 0;
-// printQuestion(questions[0]);
-//Creating an Event Listener to go through all of the questions.
 
 
-//checks if the answer is correct
+// Checks if the answer is correct
 function checkAnswer(q, a) {
     return q.answers[0] === a;
 }
 
+
+//
 function nextQuestion(q, qList){
     let currentIndex = qList.indexOf(q) + 1;
     console.log(currentIndex);
@@ -135,68 +133,42 @@ function nextQuestion(q, qList){
     }
 }
 
-//creates the intial state for the quiz
+
+// Creates the intial state for the quiz
 function initializeQuiz(qList) {
+    score = 0;
+    //creates the HTML elements for the quiz.
     createQuizElements();
     //Make a copy of the list of questions and shuffle it.
     var mixedQList = qList;
     shuffleArray(mixedQList);
-    //Makes the question box visible
-    // $("#question-box").removeClass("invisible")
     //sets the current index to 0
     var currentQIndex = 0;
-    //prints the first question
+    //Prints the first question in the shuffled list
     printQuestion(mixedQList[currentQIndex], mixedQList);
-    //creates event listener on the next question button
-    //to cycle through the different questions.
-    $("#next-question").on("click", function () {
-        currentQIndex++;
-        if (currentQIndex >= questions.length) {
-            shuffleArray(mixedQList);
-            currentQIndex = 0;
-        }
-        // console.log(currentQuestion);
-        printQuestion(questions[currentQIndex], mixedQList);
-    });
 }
 
-/*function to shuffle the questions:
-from: https://stackoverflow.com/a/12646864/13871979 */
-/* Randomize array in-place using Durstenfeld shuffle algorithm */
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
 
-function runQuiz(qList){
-    score = 0;
-    var time = maxTime;
-    //create inital state:
-    initializeQuiz(qList);
-    // startTimer(time);
-
-}
-
+// Creates the HTML elements for the quiz to display.
 function createQuizElements(){
-    //select the main div and empty it
+    // Selects the main div and empty it
     let mainDiv = $("main");
     mainDiv.empty();
-    //add the question box to the main div
+    // Add the question box to the main div
     let questionBox = $("<div>")
     questionBox.addClass("mx-auto questionBox");
     mainDiv.append(questionBox);
-    //Add the header to the question box
+    // Add the header to the question box
     let questionHeader = $("<h2>");
     questionHeader.attr("id", "question");
     questionBox.append(questionHeader);
     questionBox.append($("<hr>"));
-    //add the questions list to the question box
+    // Add the questions list to the question box
     let answersBox = $("<div>");
     answersBox.attr("id","answers-list")
     questionBox.append(answersBox);
 }
+
 
 // Ends the quiz and displays the score.
 function endQuiz(){
@@ -209,8 +181,17 @@ function setScore(){
     console.log("Current Score: " + score);
 }
 
+/* Function to shuffle the questions:
+From: https://stackoverflow.com/a/12646864/13871979 */
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
+// Allows user to run quiz on button press.
 $("#quiz-start").on("click",function(){
-    runQuiz(questions);
+    initializeQuiz(questions);
 });
-
